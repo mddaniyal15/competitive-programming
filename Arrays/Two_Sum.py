05a9366eb7fdbd11724f658799e36db52c832fb1
Mohammad Daniyal <mddaniyal.15sep@gmail.com>
2025-03-10 22:40:24 +0530

    Added Two Sum solution in python+

---
+++ Arrays/Two_Sum.py
@@ -0,0 +1,9 @@
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+    return []
+
